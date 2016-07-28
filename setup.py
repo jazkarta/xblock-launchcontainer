f236a7d981c0e6adfa945cb3645635700e058e9c
Bryan Wilson <bryanlandia@gmail.com>
2016-07-28 14:59:25 -0700

    Bump ver to 1.0.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def package_data(pkg, roots):
 
 setup(
     name='xblock-launchcontainer',
-    version='0.1',
+    version='1.0.1',
     author='Bryan Wilson, Jazkarta',
     description=('Open EdX XBlock to display a button allowing an LMS user '
                  'to launch and link to an external courseware resource via the '
